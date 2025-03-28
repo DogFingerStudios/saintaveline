cb7ee36eadd0dea631c146f97e677020ffd2428a
Addy <aclaure@gmail.com>
2025-03-27 20:38:01 -0400

    working on the door;

--- Assets/Buildings/FreeOpenBuilding_1.1.2/FOB_1/Scripts/DoorMech.cs
+++ Assets/Buildings/FreeOpenBuilding_1.1.2/FOB_1/Scripts/DoorMech.cs
@@ -22,65 +22,17 @@ public class DoorMech : MonoBehaviour
 		doorBool = false;
 		playerCamera = Camera.main;
 	}
-		
-	// void OnTriggerStay(Collider col)
-	// {
-	// 	if(col.gameObject.tag == ("Player") && Input.GetKeyDown(KeyCode.E))
-	// 	{
-	// 		if (!doorBool)
-	// 			doorBool = true;
-	// 		else
-	// 			doorBool = false;
-	// 	}
-	// }
 
 	void Update()
 	{
-		// if (Input.GetKeyDown(KeyCode.V))
-		// {
-		// 	var name = this.name;
-		// 	Debug.Log(name + " doorBool: " + doorBool);		
-		// }
-		// // doorTimer += Time.deltaTime;
-		// // if (doorTimer >= 5f)
-		// // {
-		// // 	doorBool = !doorBool;
-		// // 	doorTimer = 0f;
-		// // }
-
-        // if (Input.GetKeyDown(KeyCode.E))
-        // {
-        //     TryInteractWithDoor();
-        // }
-
-
 		if (doorBool)
 		{
-			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler (OpenRotation), rotSpeed * Time.deltaTime);
+			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(OpenRotation), rotSpeed * Time.deltaTime);
 		}
 		else
 		{
-			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler (CloseRotation), rotSpeed * Time.deltaTime);	
+			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(CloseRotation), rotSpeed * Time.deltaTime);	
 		}
 	}
-
-	// void TryInteractWithDoor()
-	// {
-	// 	Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
-	// 	RaycastHit hit;
-
-	// 	if (Physics.Raycast(ray, out hit, interactionDistance))
-	// 	{
-	// 		// // Check if the object hit by the ray has the door script
-	// 		DoorMech targetDoor = hit.collider.transform.parent != null ? hit.collider.transform.parent.GetComponent<DoorMech>() : null;
-	// 		if (targetDoor != null)
-	// 		{
-	// 			// targetDoor.ToggleDoor();  // Open or close the door
-	// 			// doorBool = !doorBool;
-	// 			targetDoor.doorBool = !targetDoor.doorBool;
-	// 		}
-	// 	}
-	// }
-
 }
 
