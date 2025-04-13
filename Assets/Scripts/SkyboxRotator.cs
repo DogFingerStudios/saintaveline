037574ccdffad3801352af24585c015ba29d8c3e
Addy <aclaure@gmail.com>
2025-04-13 11:39:57 -0400

    fix issue where the skybox keeps changing in the SCM;

--- Assets/Scripts/SkyboxRotator.cs
+++ Assets/Scripts/SkyboxRotator.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class SkyboxRotator : MonoBehaviour
 {
     public float rotationSpeed = 100.5f; // Play with this
+    private Material runtimeSkybox;
+
+    void Start()
+    {
+        // Clone the original so we don’t modify the asset
+        runtimeSkybox = new Material(RenderSettings.skybox);
+        RenderSettings.skybox = runtimeSkybox;
+    }
+
 
     void Update()
     {
-        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed);
+        runtimeSkybox.SetFloat("_Rotation", Time.time * rotationSpeed);
     }
 }
