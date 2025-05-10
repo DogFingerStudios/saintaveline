3d71de6ff517c0302cd79e30d0253bf8769b6f44
matthew-vale <matt.vale1@gmail.com>
2025-05-10 13:33:35 +0100

    Minor Optimization Pass (Cached Properties) (#11)
    * GetComponent cache for DoorMech
    
    * PlayerStats cached for GamePlayHUD
    
    * IHasHealth cached for DebugInputManager
    
    * Assigned PlayerObject for DebugInputManager

--- Assets/Buildings/FreeOpenBuilding_1.1.2/FOB_1/Scripts/DoorMech.cs
+++ Assets/Buildings/FreeOpenBuilding_1.1.2/FOB_1/Scripts/DoorMech.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DoorMech : MonoBehaviour 
 {
+    // Cached the transform. Using transform directly always
+    // uses GetComponent behind the scenes, so this script was
+    // taking up about 50% of the CPU time (at time of change) with the GetComponent call alone.
+    private Transform _transform; 
 
 	public Vector3 OpenRotation, CloseRotation;
 
@@ -16,22 +18,20 @@ public class DoorMech : MonoBehaviour
     public float interactionDistance = 3f;
     private Camera playerCamera;
 
-	void Start()
-	{
-		doorBool = false;
-		playerCamera = Camera.main;
-	}
+
+    private void Awake() {
+        _transform = transform;
+        playerCamera = Camera.main;
+
+        doorBool = false;        
+    }
 
 	void Update()
 	{
-		if (doorBool)
-		{
-			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(OpenRotation), rotSpeed * Time.deltaTime);
-		}
-		else
-		{
-			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(CloseRotation), rotSpeed * Time.deltaTime);	
-		}
+        Vector3 targetRotation = doorBool ? OpenRotation : CloseRotation;
+        if (_transform.rotation.eulerAngles != targetRotation) 
+        {
+            _transform.rotation = Quaternion.Lerp(_transform.rotation, Quaternion.Euler(targetRotation), rotSpeed * Time.deltaTime);
+        }
 	}
 }
-
