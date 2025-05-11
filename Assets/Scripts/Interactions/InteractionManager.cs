53e0702d793f9a0c80c08f396d785188923872a4
matthew-vale <matt.vale1@gmail.com>
2025-05-11 13:39:21 +0100

    Prevent opening menu if already open

--- Assets/Scripts/Interactions/InteractionManager.cs
+++ Assets/Scripts/Interactions/InteractionManager.cs
@@ -27,6 +27,12 @@ public class InteractionManager : MonoBehaviour
 
     public void OpenMenu(List<string> interactions)
     {
+        // Check if the menu is already open to prevent repeated button spawning
+        if (_buttonPanel.activeInHierarchy) 
+        {
+            return;
+        }
+
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         
