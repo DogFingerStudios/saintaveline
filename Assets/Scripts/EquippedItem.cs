c5825a733b1672963c090d43b5b06a776fdf504f
matthew-vale <matt.vale1@gmail.com>
2025-05-13 03:37:28 +0100

    Change how we pick up items

--- Assets/Scripts/EquippedItem.cs
+++ Assets/Scripts/EquippedItem.cs
@@ -1,31 +1,52 @@
 #nullable enable
+
 using UnityEngine;
 
 // this script is attached to the player character and defines where
 // items the player is holding should be placed in the world
 public class EquippedItem : MonoBehaviour
 {
-    private GameObject? _equippedItemVar;
-    public GameObject? EquippedItemVar
+    [SerializeField] private Transform _equippedItemPos;
+    private GameObject? _equippedItem;
+
+
+    // Set the equipped item, position and rotation
+    public void SetEquippedItem(GameObject itemToEquip)
     {
-        get => _equippedItemVar;
-        set => _equippedItemVar = value;
+        _equippedItem = itemToEquip;
+        _equippedItem.transform.SetParent(_equippedItemPos);
+
+        // Once parented to the item position, set the local position and rotation to zero,
+        // so it matches the item pos transform.
+        _equippedItem.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+
+        // Do we want to disable physics while we equip the item?
+        // If item doesn't have physics, we don't need to do anything
+        if (_equippedItem.TryGetComponent<Rigidbody>(out var rb)) 
+        {
+            rb.isKinematic = true;
+        }
+    }    
+
+    public void DropEquippedItem() 
+    {
+        if (_equippedItem != null) 
+        {
+            // If item has physics, re-enable them
+            if (_equippedItem.TryGetComponent<Rigidbody>(out var rb)) 
+            {
+                rb.angularVelocity = Vector3.zero;
+                rb.linearVelocity = Vector3.zero;
+                rb.isKinematic = false;
+            }
+
+            _equippedItem.transform.SetParent(null);
+            _equippedItem = null;
+        }
     }
 
-    [SerializeField] private Transform? _equippedItemPos = null;
+    // Perhaps we could listen for item drop action here? ;)
+    // ...
+    // ...
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        // nothing to do
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!_equippedItemVar || !_equippedItemPos) return;
-
-        _equippedItemVar!.transform.position = _equippedItemPos.position;
-        _equippedItemVar!.transform.rotation = _equippedItemPos.rotation;
-    }
 }
