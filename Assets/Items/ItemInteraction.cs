89e51f0d6869309aa6d710fabfbe85ea31a436ba
matthew-vale <matt.vale1@gmail.com>
2025-05-14 00:05:44 +0100

    Player Hierarchy and Equipped Item Improvements (#15)
    * Change how we pick up items
    
    * Resolved conflicts, player is now a prefab

--- Assets/Items/ItemInteraction.cs
+++ Assets/Items/ItemInteraction.cs
@@ -64,7 +64,7 @@ public class ItemInteraction : MonoBehaviour, Interactable
             return;
         }
 
-        _equippedItemScript.EquippedItemVar = this.gameObject;
+        _equippedItemScript.SetEquippedItem(this.gameObject);
     }
 
 
