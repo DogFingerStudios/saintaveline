3395d30a727af680cfafb1536856b93f3ed08755
Matthew Vale <matt.vale1@gmail.com>
2025-05-22 23:20:26 +0100

    Lamp shadows and flickering applied to prefab (#28)
    * Lamp shadows and flickering applied to prefab
    
    * Improved near plane
    
    * Fixed light no resetting
    
    * Corrected flicker enabling
    
    * Ok ok, ACTUALLY fixed the light this time

--- Assets/Scripts/Interactions/LampInteractable.cs
+++ Assets/Scripts/Interactions/LampInteractable.cs
@@ -1,21 +1,49 @@
+using System.Collections;
 using UnityEngine;
 
 public class LampInteractable : MonoBehaviour, Interactable
 {
+    public Light Light;
+
     public string HelpText
     {
         get { return "Press [E] to toggle lamp"; }
     }
 
-    // public AudioSource audioSource;
     public AudioClip lampSwitchSound;
+
+    [Header("Flickering Settings")]
+    public float minTurnOnDelay = 0f;
+    public float maxTurnOnDelay = 2f;
+    public float minIntensity = 0.8f;
+    public float maxIntensity = 1.2f;
+    public float lightChangeSpeed = 0.5f;
+    private float targetIntensity;
+    public float minFlickerTime = 0.1f;
+    public float maxFlickerTime = 1f;
+
+
+    private void Awake()
+    {
+        Light.enabled = false;
+    }
+
     public void Interact()
     {
-        // Debug.Log("Lamp " + gameObject.name + " toggled!");
+        if (Light == null)
+        {
+            Debug.Log("No Light component found on LampInteractable, should probably have one.");
+            return;
+        }
 
-        Light bulb = GetComponentInChildren<Light>();
-        if (bulb == null) return;
-        bulb.intensity = bulb.intensity == 0 ? 1 : 0;
+        if (!Light.enabled)
+        {
+            EnableLightFlicker();            
+        }
+        else
+        {
+            DisableLightFlicker();
+        }
 
         AudioSource audioSource = GetComponent<AudioSource>();
         if (lampSwitchSound != null && audioSource != null)
@@ -33,4 +61,36 @@ public class LampInteractable : MonoBehaviour, Interactable
     {
         // Debug.Log("Defocused from lamp " + gameObject.name);
     }
+
+    #region Flickering
+
+    private void EnableLightFlicker()
+    {
+        Light.enabled = true;
+        StartCoroutine(FlickerLight());
+    }
+
+    private void DisableLightFlicker()
+    {
+        Light.enabled = false;
+        StopAllCoroutines();
+    }
+
+    private IEnumerator FlickerLight()
+    {
+        targetIntensity = Random.Range(minIntensity, maxIntensity);
+        yield return new WaitForSeconds(Random.Range(minFlickerTime, maxFlickerTime));
+        StartCoroutine(FlickerLight());
+    }
+
+    private void Update()
+    {
+        if (Light.enabled)
+        {
+            Light.intensity = Mathf.Lerp(Light.intensity, targetIntensity, lightChangeSpeed * Time.deltaTime);
+        }
+    }
+
+    #endregion
+
 }
