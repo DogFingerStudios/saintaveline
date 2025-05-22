e30009d04ffeebc0d21ca251ea7b905c392be306
matthew-vale <matt.vale1@gmail.com>
2025-05-22 02:26:23 +0100

    Lamp shadows and flickering applied to prefab

--- Assets/Scripts/Interactions/LampInteractable.cs
+++ Assets/Scripts/Interactions/LampInteractable.cs
@@ -1,21 +1,46 @@
+using System.Collections;
 using UnityEngine;
 
 public class LampInteractable : MonoBehaviour, Interactable
 {
+    public Light Light;
+
     public string HelpText
     {
         get { return "Press [E] to toggle lamp"; }
     }
 
-    // public AudioSource audioSource;
     public AudioClip lampSwitchSound;
+
+    [Header("Flickering Settings")]
+    public float minTurnOnDelay = 0f;
+    public float maxTurnOnDelay = 2f;
+    public float minIntensity = 0.8f;
+    public float maxIntensity = 1.2f;
+    public float lightChangeSpeed = 0.5f;
+    private float targetIntensity;
+    public float minFlickerTime = 0.1f;
+    public float maxFlickerTime = 1f;
+
+
     public void Interact()
     {
-        // Debug.Log("Lamp " + gameObject.name + " toggled!");
+        if (Light == null)
+        {
+            Debug.Log("No Light component found on LampInteractable, should probably have one.");
+            return;
+        }
 
-        Light bulb = GetComponentInChildren<Light>();
-        if (bulb == null) return;
-        bulb.intensity = bulb.intensity == 0 ? 1 : 0;
+        Light.intensity = Light.intensity == 0 ? 1 : 0;
+
+        if (Light.intensity == 0)
+        {
+            DisableLightFlicker();
+        }
+        else
+        {
+            EnableLightFlicker();
+        }
 
         AudioSource audioSource = GetComponent<AudioSource>();
         if (lampSwitchSound != null && audioSource != null)
@@ -33,4 +58,34 @@ public class LampInteractable : MonoBehaviour, Interactable
     {
         // Debug.Log("Defocused from lamp " + gameObject.name);
     }
+
+    #region Flickering
+
+    private void EnableLightFlicker()
+    {
+        StartCoroutine(FlickerLight());
+    }
+
+    private void DisableLightFlicker()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator FlickerLight()
+    {
+        targetIntensity = Random.Range(minIntensity, maxIntensity);
+        yield return new WaitForSeconds(Random.Range(minFlickerTime, maxFlickerTime));
+        StartCoroutine(FlickerLight());
+    }
+
+    private void Update()
+    {
+        if (Light.enabled)
+        {
+            Light.intensity = Mathf.Lerp(Light.intensity, targetIntensity, lightChangeSpeed * Time.deltaTime);
+        }
+    }
+
+    #endregion
+
 }
