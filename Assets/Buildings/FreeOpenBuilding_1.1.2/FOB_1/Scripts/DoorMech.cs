3e41f17493cc2620862b5cdb7627e40a033e71f4
Matthew Vale <matt.vale1@gmail.com>
2025-05-25 12:38:36 +0100

    Door location rotation (#37)

--- Assets/Buildings/FreeOpenBuilding_1.1.2/FOB_1/Scripts/DoorMech.cs
+++ Assets/Buildings/FreeOpenBuilding_1.1.2/FOB_1/Scripts/DoorMech.cs
@@ -1,37 +1,29 @@
 ﻿using UnityEngine;
 
-public class DoorMech : MonoBehaviour 
+public class DoorMech : MonoBehaviour
 {
     // Cached the transform. Using transform directly always
     // uses GetComponent behind the scenes, so this script was
     // taking up about 50% of the CPU time (at time of change) with the GetComponent call alone.
-    private Transform _transform; 
-
-	public Vector3 OpenRotation, CloseRotation;
-
-	public float rotSpeed = 1f;
-
-	public bool doorBool;
-
-	// private float doorTimer = 0f;
-
+    private Transform _transform;
+    public Vector3 OpenRotation, CloseRotation;
+    public float rotSpeed = 1f;
+    public bool doorBool;
     public float interactionDistance = 3f;
-    private Camera playerCamera;
 
 
-    private void Awake() {
+    private void Awake()
+    {
         _transform = transform;
-        playerCamera = Camera.main;
-
-        doorBool = false;        
+        doorBool = false;
     }
 
-	void Update()
-	{
+    void Update()
+    {
         Vector3 targetRotation = doorBool ? OpenRotation : CloseRotation;
-        if (_transform.rotation.eulerAngles != targetRotation) 
+        if (_transform.localRotation.eulerAngles != targetRotation)
         {
-            _transform.rotation = Quaternion.Lerp(_transform.rotation, Quaternion.Euler(targetRotation), rotSpeed * Time.deltaTime);
+            _transform.localRotation = Quaternion.Lerp(_transform.localRotation, Quaternion.Euler(targetRotation), rotSpeed * Time.deltaTime);
         }
-	}
+    }
 }
