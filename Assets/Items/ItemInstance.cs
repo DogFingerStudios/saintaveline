a19f09700598c30ef408f04ad769c7c58313b062
Addy <aclaure@gmail.com>
2025-06-10 16:13:43 -0400

    yay

--- Assets/Items/ItemInstance.cs
+++ Assets/Items/ItemInstance.cs
@@ -1,14 +1,14 @@
 using System;
 
-[System.Serializable]
-public class ItemInstance
-{
-    public ItemData Data;
-    public float Condition;
+// [System.Serializable]
+// public class ItemInstance
+// {
+//     public ItemData Data;
+//     public float Condition;
 
-    public ItemInstance(ItemData data)
-    {
-        Data = data;
-        Condition = 1.0f;
-    }
-}
+//     public ItemInstance(ItemData data)
+//     {
+//         Data = data;
+//         Condition = 1.0f;
+//     }
+// }
---
+++ Assets/Items/PistolInteraction.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class PistolInteraction : ItemInteraction
+{
+
+}
