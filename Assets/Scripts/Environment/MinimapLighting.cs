d7cecd3efd41e2079cd31f0bd2f292a07ddb77d0
Addy <aclaure@gmail.com>
2025-10-18 15:32:41 -0400

    more housekeeping;

--- Assets/Scripts/Environment/MinimapLighting.cs
+++ Assets/Scripts/Environment/MinimapLighting.cs
@@ -4,11 +4,31 @@ using UnityEngine.Rendering;
 public class MinimapLighting : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
-    [SerializeField] private Light _globalLight;
+    [SerializeField] private Light _minimapLight;
+    [SerializeField] private Transform _target; // player transform
 
-    // Update is called once per frame
+    private float _minSize = 10f;
+    private float _maxSize = 500f;
+    private float _zoomSpeed = 10f;
+
     void Update()
     {
+        Vector3 newPosition = _target.transform.position;
+        newPosition.y = 100f;
+        this.transform.position = newPosition;
+
+        if (Input.GetKeyDown(KeyCode.Minus) && _camera.orthographicSize < _maxSize)
+        {
+            _camera.orthographicSize += _zoomSpeed;
+        }
+        else if (Input.GetKeyDown(KeyCode.Equals) && _camera.orthographicSize > _minSize)
+        {
+            _camera.orthographicSize -= _zoomSpeed;
+        }
+
+        //Vector3 newRotation = this.transform.eulerAngles;
+        //newRotation.y = _target.transform.eulerAngles.y;
+        //this.transform.eulerAngles = newRotation;
     }
 
     void OnEnable()
@@ -27,7 +47,7 @@ public class MinimapLighting : MonoBehaviour
     {
         if (cam == _camera)
         {
-            _globalLight.enabled = true;
+            _minimapLight.enabled = true;
         }
     }
 
@@ -35,7 +55,7 @@ public class MinimapLighting : MonoBehaviour
     {
         if (cam == _camera)
         {
-            _globalLight.enabled = false;
+            _minimapLight.enabled = false;
         }
     }
 }
