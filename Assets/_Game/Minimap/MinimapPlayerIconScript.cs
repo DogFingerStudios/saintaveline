312e03062a1646c7665760657552baee4cad429a
Addy <aclaure@gmail.com>
2025-10-20 20:31:06 -0400

    finished implementing the player minimap icon;

--- Assets/_Game/Minimap/MinimapPlayerIconScript.cs
+++ Assets/_Game/Minimap/MinimapPlayerIconScript.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class MinimapPlayerIcon : MonoBehaviour
 {
-    private Transform _playerTransform;
-
-    void Start()
-    {
-        _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-    }
+    [SerializeField] Camera _minimapCamera;
 
     void Update()
     {
-        
+        Vector3 newPosition = _minimapCamera.transform.position;
+        newPosition.y -= 10;
+        transform.position = newPosition;
+
+        float newScale = _minimapCamera.orthographicSize * 0.125f;
+        transform.localScale = new Vector3(newScale, newScale, newScale);
     }
 }
