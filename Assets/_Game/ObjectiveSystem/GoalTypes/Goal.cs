c8e2274c5e3fed07c34892d2794a10e3701c8fc2
Addy <aclaure@gmail.com>
2025-10-25 20:40:08 -0400

    moving things into individual files because Matthew is a wanker;

---
+++ Assets/_Game/ObjectiveSystem/GoalTypes/Goal.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewGoal", menuName = "Game/Goals/Goal")]
+public class GoalSO : ScriptableObject
+{
+    public string Name;
+    [TextArea] public string Description;
+    public string StartMessage;
+    public string SuccessMessage;
+    public string FailureMessage;
+}
+
+public class Goal
+{
+    private readonly GoalSO Data;
+    public T TypedData<T>() => (T)(object)Data;
+
+    // public setter allows for the player to assign the goal/task to someone else,
+    // though we'll probably eventually want a setter function, as there may be
+    // things to do when assigning a new host (like updating UI, etc).
+    public CharacterEntity? Host { get; set; }
+
+    public string Name => Data.Name;
+    public string Description => Data.Description;
+
+    public event Action? OnCompleted;
+
+    public Goal(GoalSO data)
+    {
+        Data = data;
+    }
+
+    public void Complete()
+    {
+        OnCompleted?.Invoke();
+    }
+
+    public virtual void ManualUpdate()
+    {
+        // nothing to do
+    }
+}
