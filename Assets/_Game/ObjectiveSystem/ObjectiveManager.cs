35ed8ae10be20dad8871554857085bf7d06f6cd7
Addy <aclaure@gmail.com>
2025-10-28 06:50:02 -0400

    consolidated the `ObjectiveSystem` class into the `ObjectiveManager` class;

--- Assets/_Game/ObjectiveSystem/ObjectiveManager.cs
+++ Assets/_Game/ObjectiveSystem/ObjectiveManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,21 +9,65 @@ public class ObjectiveManager : MonoBehaviour
     [SerializeField] private GameObject Minimap;
     [SerializeField] private ObjectiveSO InitialObjective;
 
-    private ObjectiveSystem _objectiveSystem = ObjectiveSystem.Instance;
+    //private ObjectiveSystem _objectiveSystem = ObjectiveSystem.Instance;
+    private Objective? CurrentObjective;
+    private RunOnce? _runonce;
 
     public void Awake()
     {
         var player = GameObject.FindWithTag("Player");
         var entity = player.GetComponent<CharacterEntity>();
 
-        _objectiveSystem.CurrentObjective =
+        CurrentObjective =
             ObjectiveFactory.Instance.CreateObjectiveFromSO(InitialObjective, entity);
 
-        _objectiveSystem.ManualAwake();
+        if (CurrentObjective == null) return;
+
+        CurrentObjective.OnObjectiveCompleted += ObjectiveCompleteHandler;
+        CurrentObjective.ManualAwake();
+
+        if (!CurrentObjective.StartMessage.Equals(string.Empty))
+        {
+            _runonce = new RunOnce()
+            {
+                PreCalls = 1,
+                Func = () =>
+                {
+                    BottomTypewriter.Instance.Enqueue(CurrentObjective.StartMessage);
+
+                    if (CurrentObjective.CurrentGoal != null &&
+                        !CurrentObjective.CurrentGoal.StartMessage.Equals(string.Empty))
+                    {
+                        BottomTypewriter.Instance.Enqueue(CurrentObjective.CurrentGoal.StartMessage);
+                    }
+                }
+            };
+        }
     }
 
     void Update()
     {
-        _objectiveSystem.ManualUpdate(this);
+        _runonce?.Run();
+        if (CurrentObjective == null) return;
+
+        CurrentObjective.ManualUpdate();
+    }
+
+    void ObjectiveCompleteHandler()
+    {
+        if (CurrentObjective == null)
+        {
+            throw new Exception("CurrentObjective is null in ObjectiveCompleteHandler.");
+        }
+
+        string msg = $"Completed objective '{CurrentObjective.Name}'";
+        Debug.Log(msg);
+
+        if (!CurrentObjective.SuccessMessage.Equals(string.Empty))
+        {
+            BottomTypewriter.Instance.Enqueue(CurrentObjective.SuccessMessage);
+        }
+
+        CurrentObjective = null;
     }
 }
