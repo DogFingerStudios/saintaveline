25accc8b7bef8b010c342dee40d2e9a76111f4e7
matthewvale <matt.vale1@gmail.com>
2025-10-29 23:50:04 +0000

    Update SceneLoader.cs

--- Assets/_Game/Scripts/UI/SceneLoader.cs
+++ Assets/_Game/Scripts/UI/SceneLoader.cs
@@ -1,28 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-#if UNITY_EDITOR
-    using UnityEditor;
-#endif
-
 public class SceneLoader : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
+        Debug.LogError("<color=green>Loading scene: " + sceneName + "</color>");
         SceneManager.LoadScene(sceneName);
     }
 
     public void QuitGame()
     {
-#if UNITY_EDITOR
-        EditorApplication.isPlaying = false;
-#else
         Application.Quit();
-#endif
     }
 
     public void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
 }
