04f420a21a9cc3ae1f7ff8dcae775135baca0828
Addy <aclaure@gmail.com>
2025-10-28 20:46:34 -0400

    HOLY FUCKING SHIT WHY DID THIS TAKE SO FUCKING LONG

--- Assets/_Game/ObjectiveSystem/ObjectiveManager.cs
+++ Assets/_Game/ObjectiveSystem/ObjectiveManager.cs
@@ -9,12 +9,35 @@ public class ObjectiveManager : MonoBehaviour
     [SerializeField] private GameObject Minimap;
     [SerializeField] private ObjectiveSO InitialObjective;
 
+    // we assume that the minimap camera is a child of the minimap object
+    private Camera _minimapCamera;
+
     //private ObjectiveSystem _objectiveSystem = ObjectiveSystem.Instance;
     private Objective? CurrentObjective;
     private RunOnce? _runonce;
 
+    GameObject _activeGoalIcon;
+
+    private void RefactorMeButThisWillDoForNow()
+    {
+        var goaldata = CurrentObjective?.CurrentGoal?.TypedData<ArriveAtGoalSO>();
+        if (goaldata == null) return;
+
+        _activeGoalIcon = Instantiate(goaldata.MinimapIcon, goaldata.Location, goaldata.MinimapIcon.transform.rotation);
+        //_activeGoalIcon.transform.localScale *= 10;
+        //_activeGoalIcon.layer = LayerMask.GetMask("MinimapLayer");
+
+        Debug.Log("The size of the _activeGoalIcon is" + _activeGoalIcon.transform.localScale);
+    }
+
     public void Awake()
     {
+        _minimapCamera = Minimap.GetComponentInChildren<Camera>();
+        if (_minimapCamera == null)
+        {
+            throw new Exception("Minimap camera not found as child of Minimap GameObject.");
+        }
+
         var player = GameObject.FindWithTag("Player");
         var entity = player.GetComponent<CharacterEntity>();
 
@@ -40,6 +63,8 @@ public class ObjectiveManager : MonoBehaviour
                     {
                         BottomTypewriter.Instance.Enqueue(CurrentObjective.CurrentGoal.StartMessage);
                     }
+
+                    RefactorMeButThisWillDoForNow();
                 }
             };
         }
@@ -51,6 +76,11 @@ public class ObjectiveManager : MonoBehaviour
         if (CurrentObjective == null) return;
 
         CurrentObjective.ManualUpdate();
+
+        var goaldata = CurrentObjective?.CurrentGoal?.TypedData<ArriveAtGoalSO>();
+        Vector3 newPosition = goaldata.Location;
+        newPosition.y = _minimapCamera.transform.position.y - 10;
+        _activeGoalIcon.transform.position = newPosition;
     }
 
     void ObjectiveCompleteHandler()
