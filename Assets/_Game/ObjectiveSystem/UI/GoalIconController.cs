616f9bd052e53d6209998b1c3ff55feb5e865da4
Matthew Vale <matt.vale1@gmail.com>
2025-10-29 11:48:53 +0000

    Minimap UI magic (#155)

---
+++ Assets/_Game/ObjectiveSystem/UI/GoalIconController.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoalIconController : MonoBehaviour
+{
+    [Tooltip("The UI version of this sprite to be used on the minimap.")]
+    public GameObject GoalIconPrefab;
+
+    [Tooltip("Offset radius for the minimap icon from the edge of the minimap.")]
+    public float MinimapRadiusOffset = 0f;
+
+    private Camera _minimapCamera;
+    private RectTransform _minimapParent;
+    private SpriteRenderer _goalIconInstance;
+
+    private GameObject _goalIconMinimapInstance;
+    private RectTransform _minimapGoalIconRect;
+    private float _minimapTransformRadius;
+
+
+    public void SetData(Camera minimapCamera, RectTransform minimapParent)
+    {
+        _minimapCamera = minimapCamera;
+        _minimapParent = minimapParent;
+
+        // Calculate the minimap radius based on the RectTransform size
+        _minimapTransformRadius = _minimapParent.rect.width / 2f;
+        _minimapTransformRadius += MinimapRadiusOffset;
+
+        // Grab the world space goal icon
+        _goalIconInstance = GetComponent<SpriteRenderer>();
+
+        // Spawn the constrained minimap icon
+        _goalIconMinimapInstance = Instantiate(GoalIconPrefab, _minimapParent.position, Quaternion.identity, _minimapParent);
+        _goalIconMinimapInstance.GetComponent<Image>().sprite = _goalIconInstance.sprite;
+        _goalIconMinimapInstance.GetComponent<Image>().color = _goalIconInstance.color;
+
+        // Get the RectTransform for positioning
+        _minimapGoalIconRect = _goalIconMinimapInstance.GetComponent<RectTransform>();
+    }
+
+    private void LateUpdate()
+    {
+        // Update the position of the minimap icon to stay within bounds of the minimap radius
+        if (_goalIconMinimapInstance != null)
+        {
+            Vector3 viewportPos = _minimapCamera.WorldToViewportPoint(transform.position);
+
+            // Convert viewport position to minimap position
+            Vector2 minimapPos = new(
+                (viewportPos.x - _minimapParent.pivot.x) * _minimapParent.rect.width,
+                (viewportPos.y - _minimapParent.pivot.y) * _minimapParent.rect.height
+            );
+
+            // Calculate the distance from the center
+            float distanceFromCenter = minimapPos.magnitude;
+
+            // If the icon is outside the minimap radius, clamp it
+            if (distanceFromCenter > _minimapTransformRadius)
+            {
+                _goalIconMinimapInstance.SetActive(true);
+                minimapPos = minimapPos.normalized * _minimapTransformRadius;
+            }
+            else
+            {
+                _goalIconMinimapInstance.SetActive(false);
+            }
+
+            _minimapGoalIconRect.anchoredPosition = minimapPos;
+        }
+    }
+
+    void OnDisable()
+    {
+        // Destroy the constrained minimap icon
+        if (_goalIconMinimapInstance != null)
+        {
+            Destroy(_goalIconMinimapInstance);
+        }
+    }
+
+}
