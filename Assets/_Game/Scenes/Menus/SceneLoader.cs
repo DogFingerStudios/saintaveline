473667af9647b96f8288cf8871b11824d063c108
matthewvale <matt.vale1@gmail.com>
2025-10-30 01:23:29 +0000

    Organised the menu scenes and scripts

--- Assets/_Game/Scenes/Menus/SceneLoader.cs
+++ Assets/_Game/Scenes/Menus/SceneLoader.cs
@@ -1,19 +1,30 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Scripting;
 
 public class SceneLoader : MonoBehaviour
 {
+    [Preserve]
     public void LoadScene(string sceneName)
     {
-        Debug.LogError("<color=green>Loading scene: " + sceneName + "</color>");
+        Debug.Log("<color=green>Loading scene: " + sceneName + "</color>");
         SceneManager.LoadScene(sceneName);
     }
 
+    [UsedImplicitly]
     public void QuitGame()
     {
+        Debug.Log("<color=red>Quitting game...</color>");
         Application.Quit();
     }
 
+    private void Update()
+    {
+        Debug.Log("<color=blue>Holy hell wtf</color>");
+    }
+
+    [UsedImplicitly]
     public void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
