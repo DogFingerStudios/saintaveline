3af683c8712c2b5f6f0825a0a00bbf7fbb42c60f
Addy <aclaure@gmail.com>
2025-10-31 09:05:12 -0400

    move async goal handler code into its own file;

--- Assets/_Game/MissionSystem/GoalHandlerAsync.cs
+++ Assets/_Game/MissionSystem/GoalHandlerAsync.cs
@@ -1,16 +1,37 @@
-using UnityEngine;
+using System.Collections.Generic;
 
-// public class GoalHandlerAsync : MonoBehaviour
-// {
-//     // Start is called once before the first execution of Update after the MonoBehaviour is created
-//     void Start()
-//     {
-        
-//     }
+public class GoalHandlerAsync : GoalHandlerBase
+{
+    List<Goal> _inProcessGoals = new();
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-        
-//     }
-// }
+    public override void StartMission()
+    {
+        foreach (var goal in Goals)
+        {
+            _inProcessGoals.Add(goal);
+            goal.OnGoalStarted += base.NotifyGoalStarted;
+            goal.OnGoalCompleted += GoalCompletedHandler;
+            goal.Start();
+        }
+    }
+
+    void GoalCompletedHandler(Goal goal)
+    {
+        NotifyGoalCompleted(goal);
+
+        _inProcessGoals.Remove(goal);
+
+        if (_inProcessGoals.Count == 0)
+        {
+            NotifyAllGoalsCompleted();
+        }
+    }
+
+    public override void ManualUpdate()
+    {
+        foreach (var goal in _inProcessGoals)
+        {
+            goal.ManualUpdate();
+        }
+    }
+}
--- Assets/_Game/MissionSystem/GoalHandlerBase.cs
+++ Assets/_Game/MissionSystem/GoalHandlerBase.cs
@@ -30,38 +30,3 @@ public abstract class GoalHandlerBase
     public abstract void ManualUpdate();
 }
 
-public class GoalHandlerAsync : GoalHandlerBase
-{
-    List<Goal> _inProcessGoals = new();
-
-    public override void StartMission()
-    {
-        foreach (var goal in Goals)
-        {
-            _inProcessGoals.Add(goal);
-            goal.OnGoalStarted += base.NotifyGoalStarted;
-            goal.OnGoalCompleted += GoalCompletedHandler;
-            goal.Start();
-        }
-    }
-
-    void GoalCompletedHandler(Goal goal)
-    {
-        NotifyGoalCompleted(goal);
-
-        _inProcessGoals.Remove(goal);
-
-        if (_inProcessGoals.Count == 0)
-        {
-            NotifyAllGoalsCompleted();
-        }
-    }
-
-    public override void ManualUpdate()
-    {
-        foreach (var goal in _inProcessGoals)
-        {
-            goal.ManualUpdate();
-        }
-    }
-}
