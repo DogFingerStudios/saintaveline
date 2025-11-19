11b0c5dc79e5b4403a78984fe646c10bed7ea878
Addy <aclaure@gmail.com>
2025-11-18 19:42:47 -0800

    goth;

---
+++ Assets/_Game/Entitties/Bouncer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Bouncer : MonoBehaviour
+{
+    public float moveDistance = 2f; // how far up/down
+    public float moveDuration = 2f; // total time for up + down
+
+    private Vector3 startPos;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        float t = Mathf.PingPong(Time.time, moveDuration) / moveDuration;
+        float yOffset = Mathf.Lerp(-moveDistance, moveDistance, t);
+        transform.position = startPos + new Vector3(0, yOffset, 0);
+    }
+
+    public void SetVisible(bool isVisible)
+    {
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.enabled = isVisible;
+        }
+    }
+}
