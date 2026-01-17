ab832d9a7d4883668240b40825448c7f06a48344
Matthew Vale <matt.vale1@gmail.com>
2026-01-17 13:36:38 +0000

    BoatDriver now Inits in Start

--- Assets/_ThirdParty/HQ Boats/6.scripts/BoatDriver.cs
+++ Assets/_ThirdParty/HQ Boats/6.scripts/BoatDriver.cs
@@ -79,7 +79,7 @@ public class BoatDriver : MonoBehaviour
     private FootstepAudio? _footstepAudio = null!;
     private CharacterController? _characterController = null!;
 
-    private void Awake()
+    private void Start()
     {
         var playerObject = GameObject.FindGameObjectWithTag("Player");
         _fpsMovement = playerObject.GetComponent<FPSMovement>();
