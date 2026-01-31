34bf5f61dfa2a6714d27e7cb88eda1bb0c91d573
Addy <aclaure@gmail.com>
2026-01-31 09:49:44 -0500

    Adding a description to Zones;

--- Assets/_Game/Scripts/Environment/Zone.cs
+++ Assets/_Game/Scripts/Environment/Zone.cs
@@ -5,7 +5,12 @@ public class Zone : MonoBehaviour
     [SerializeField] private string _zoneName;
     [SerializeField] private int _priority;
 
+    [SerializeField]
+    [TextArea]
+    private string _zoneDescription;
+
     public string ZoneName => _zoneName;
+    public string ZoneDescription => _zoneDescription;
     public int Priority => _priority;
 
     private void OnValidate()
