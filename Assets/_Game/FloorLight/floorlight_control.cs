29f4e48edd7b03a325625483f153821ac92168f2
Matthew Vale <matt.vale1@gmail.com>
2026-02-28 18:01:22 +0000

    💡 Flickering Light Improvements (#206)
    * Update PPVolume.asset
    
    * Floor light prefab cleanup
    
    * Applied light prefab fixes at scene level

--- Assets/_Game/FloorLight/floorlight_control.cs
+++ Assets/_Game/FloorLight/floorlight_control.cs
@@ -1,25 +1,18 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Renderer))]
+//[RequireComponent(typeof(Renderer))]
 public class floorlight_control : MonoBehaviour
 {
-    Material _lightMaterial = null!;
-    Light _floorLight;
+    //Material _lightMaterial;
+    //Light _floorLight;
 
-    void Start()
-    {
-        _floorLight = GetComponent<Light>();
-        
-        _lightMaterial = GetComponent<Renderer>().material;
-        // _lightMaterial.SetColor("_Emission", Color.green);
-        // _lightMaterial.SetColor("_BaseColor", Color.green);
-        // .SetColor("_Emission", colorVariable);
-
-        // _floorLight.color = Color.green;
-    }
-
-    void Update()
-    {
-        
-    }
+    //void Start()
+    //{
+    //    _floorLight = GetComponent<Light>();
+    //    _lightMaterial = GetComponent<Renderer>().material;
+    //    _lightMaterial.SetColor("_Emission", Color.green);
+    //    _lightMaterial.SetColor("_BaseColor", Color.green);
+    //     .SetColor("_Emission", colorVariable);
+    //    _floorLight.color = Color.green;
+    //}
 }
