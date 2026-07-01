5634226baf9104372ac0738dab7bedab7c928f1f
Addy <aclaure@gmail.com>
2026-07-01 13:33:31 +0200

    beginning implementation of saving gamestate;

--- Assets/_Game/Scripts/UI/MainMenuController.cs
+++ Assets/_Game/Scripts/UI/MainMenuController.cs
@@ -1,10 +1,32 @@
 using UnityEngine;
+using System.IO;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] public GameObject continueButton = null!;
+
+    private string _saveDirectoryPath = null!;
+
+    void Awake()
+    {
+        _saveDirectoryPath = Path.Combine(Application.persistentDataPath, "Saves");
+
+        if (!Directory.Exists(_saveDirectoryPath))
+        {
+            Directory.CreateDirectory(_saveDirectoryPath);
+        }
+
+        Debug.Log($"Save directory path: {_saveDirectoryPath}");
+    }
+
     void Start()
     {
+        Debug.Log($"Number of files in save directory: {Directory.GetFiles(_saveDirectoryPath).Length}");
+        if (Directory.GetFiles(_saveDirectoryPath).Length > 0)
+        {
+            continueButton.SetActive(true);
+        }
+
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
